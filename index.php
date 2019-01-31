50557b63acf358415c0b4dc8603a64b6ba09d2ba
Anel Poturovic <Anel@Anels-MacBook-Pro.local>
2019-01-31 10:53:42 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CV</title>
+</head>
+<body>
+        <form action="" method="post">  
+        First Name: <input type="text" name="first_name"> <br>
+        Last Name: <input type="text"  name="last_name"> <br>
+        E-mail: <input type="text" name="email"> <br>
+        Message: <br> <textarea rows="5" name="message" cols="30"></textarea>
+        <br>
+        <input type="submit" name="submit" value="Submit">
+        </form>
+
+</body>
+</html>
